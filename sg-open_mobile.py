538f76fbda0aa38944d7e5a3e368dcae9d9cd205
toto505 <hauabusakomachi@gmail.com>
2021-12-17 21:08:04 +0900

    20211217

---
+++ sg-open_mobile.py
@@ -0,0 +1,24 @@
+#v1.0
+from kivy.app import App
+from kivy.uix.button import Button
+
+import webbrowser
+import random
+url="https://www.sg-siken.com/s/kakomon/"
+url_year=[28,29,30,31,1]
+url_serson=int(1)
+mon = [1,2,3]
+if url_serson==1:
+    url_serson=str('haru')
+else:
+    url_serson=str('aki')
+url_year=random.choice(url_year)
+mon=random.choice(mon)
+url_year='%02d' % url_year
+mon='%02d' % mon
+if url_year=='01':
+    url_serson='aki'
+elif url_year=='31':
+    url_serson='haru'
+result=url + url_year + "_" + url_serson + "/pm" + mon + ".html"
+webbrowser.open(result)
